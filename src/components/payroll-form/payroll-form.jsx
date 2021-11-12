4b5d333554d7576db3e01b5f2855dc50e8cab66c
varshini <dreamvarshini@gmail.com>
2021-11-12 18:07:47 +0530

    ADDED: display message for submitting

--- src/components/payroll-form/payroll-form.jsx
+++ src/components/payroll-form/payroll-form.jsx
@@ -7,6 +7,7 @@ import './payroll-form.scss';
 import logo from '../../assets/images/logo.png'
 import { useParams,Link,withRouter } from 'react-router-dom';
 import EmployeeService from '../../services/employee-service.js';
+import DisplayMessage from '../displayMessage/display-message';
 
 var employee = new EmployeeService();
 const PayrollForm=(props)=>{
@@ -48,6 +49,8 @@ const PayrollForm=(props)=>{
     }
 
     const [formValue,setForm]=useState(initialValue);
+    const [displayMessage,setDisplayeMessage]=useState("");
+
 
     const changeValue=(event)=>{
         setForm({...formValue,[event.target.name]:event.target.value})
@@ -98,6 +101,11 @@ const PayrollForm=(props)=>{
             error.salary='salary is required field'
             isError=true;
         }
+        if( parseInt(formValue.salary) <30000 || parseInt(formValue.salary) >50000)
+        {
+            error.salary='salary should be between 30,000 and 50,000'
+            isError=true;
+        }
 
         if(formValue.profileUrl.length<1)
         {
@@ -139,15 +147,19 @@ const PayrollForm=(props)=>{
 
         employee.addEmployee(object).then(data=>{
             console.log("data added");
-        }).catch(err =>{
+            setDisplayeMessage("Successfullly added User")
+            setTimeout(()=>{
+                window.location.reload();},3000);
+            
+        })
+        .catch(err =>{
             console.log("err while add");
+            setDisplayeMessage("Error while  adding")
+
         })
 
     }
 
-  
-
-   
 
     const reset=()=>{
         setForm({...initialValue,id:formValue.id,isUpdate: formValue.isUpdate})
@@ -218,12 +230,12 @@ const PayrollForm=(props)=>{
                         <label className="label text" htmlFor="department">Department</label>
                         <div>
                             {formValue.allDepartment.map(item=>(
-                                <span key={item}>
+                                <span className="deptlabel" key={item}>
 
                                     <input className= "checkbox " type="checkbox" onChange={()=> onCheckChange(item)} name={item}
                                     defaultChecked={()=>getChecked(item)} value={item}/>
 
-                                    <label className="text" htmlFor={item}>{item}</label>
+                                    <label className="text " htmlFor={item}>{item}</label>
                                 </span>
                             ))}
                         </div>
@@ -238,10 +250,63 @@ const PayrollForm=(props)=>{
                     </div>
                     <div className="error">{formValue.error.salary}</div>
                     <div className="row"><label className="label text" htmlFor="startDate">Start date</label><div>
-                        <select onChange={changeValue} id="day" name="day"><option value="1">1</option><option value="2">2</option></select>
-                        <select onChange={changeValue} id="month" name="month"><option value="Jan">January</option><option value="Feb">February</option></select>
+                        <select onChange={changeValue} id="day" name="day">
+                            <option value="" disabled selected>Day</option>
+                            <option value="01">1</option>
+                            <option value="02">2</option>
+                            <option value="03">3</option>
+                            <option value="04">4</option>
+                            <option value="05">5</option>
+                            <option value="06">6</option>
+                            <option value="07">7</option>
+                            <option value="08">8</option>
+                            <option value="09">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                        <select onChange={changeValue} id="month" name="month">
+                            <option value="" disabled selected>Month</option>
+                            <option value="01">January</option>
+                            <option value="02">February</option>
+                            <option value="03">March</option>
+                            <option value="04">April</option>
+                            <option value="05">May</option>
+                            <option value="06">June</option>
+                            <option value="07">July</option>
+                            <option value="08">August</option>
+                            <option value="09">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
                         <select onChange={changeValue} id="year" name="year">
+                            <option value="" disabled selected>Year</option>
+                            <option value="2021">2021</option>
                             <option value="2020">2020</option>
+                            <option value="2019">2019</option>
+                            <option value="2018">2018</option>
+                            <option value="2017">2017</option>
+                            <option value="2016">2016</option>
                         </select>
                         </div>
 
@@ -262,6 +327,7 @@ const PayrollForm=(props)=>{
                         </div>
                     </div>
 
+                    <div className="displayMessage">{displayMessage}</div>
 
                     
                 </form>
